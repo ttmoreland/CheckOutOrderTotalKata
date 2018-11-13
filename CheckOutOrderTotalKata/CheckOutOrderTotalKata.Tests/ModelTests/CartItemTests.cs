23dd4e57f1128fbb4a03cec72e10b6148e586438
ttmoreland <tristantmoreland@gmail.com>
2018-11-13 15:13:15 -0600

    Added GroceryStore object & tests, Cleaned up namespace & organized model tests.

--- CheckOutOrderTotalKata/CheckOutOrderTotalKata.Tests/ModelTests/CartItemTests.cs
+++ CheckOutOrderTotalKata/CheckOutOrderTotalKata.Tests/ModelTests/CartItemTests.cs
@@ -1,6 +1,6 @@
 ﻿using System;
 using Xunit;
-using CheckOutOrderTotalKata;
+using CheckOutOrderTotalKata.Models;
 
 namespace CheckOutOrderTotalKata.Tests
 {
---
+++ CheckOutOrderTotalKata/CheckOutOrderTotalKata.Tests/ModelTests/GroceryStoreTests.cs
@@ -0,0 +1,17 @@
+﻿using Xunit;
+using CheckOutOrderTotalKata.Models;
+using System.Collections.Generic;
+
+namespace CheckOutOrderTotalKata.Tests
+{
+    public class GroceryStoreTests
+    {
+        [Fact]
+        public void TestGroceryStoreCollections()
+        {
+            GroceryStore groceryStore = new GroceryStore();
+            Assert.Equal(new List<GroceryItem>(), groceryStore.GroceryItems);
+            Assert.Equal(new List<CartItem>(), groceryStore.CartItems);
+        }
+    }
+}
