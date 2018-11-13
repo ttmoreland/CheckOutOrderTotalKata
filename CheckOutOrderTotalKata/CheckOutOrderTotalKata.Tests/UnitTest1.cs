c44416e8d6735593ae55d14eb127c0349886b359
ttmoreland <tristantmoreland@gmail.com>
2018-11-12 21:05:19 -0600

    Initial project & tests created

---
+++ CheckOutOrderTotalKata/CheckOutOrderTotalKata.Tests/UnitTest1.cs
@@ -0,0 +1,15 @@
+using System;
+using Xunit;
+using CheckOutOrderTotalKata;
+
+namespace CheckOutOrderTotalKata.Tests
+{
+    public class UnitTest1
+    {
+        [Fact]
+        public void Test1()
+        {
+
+        }
+    }
+}
