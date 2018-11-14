b63956486a978037b67ab8c3246c41a874fc5846
ttmoreland <tristantmoreland@gmail.com>
2018-11-14 15:21:42 -0600

    Renaming test to reflect renaming of Model

---
+++ CheckOutOrderTotalKata/CheckOutOrderTotalKata.Tests/ModelTests/StoreItemTests.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using Xunit;
+using CheckOutOrderTotalKata.Models;
+
+namespace CheckOutOrderTotalKata.Tests
+{
+    public class StoreItemTests
+    {
+        [Fact]
+        public void StoreItem_TestEachItemProperties()
+        {
+            StoreItem storeItem = new StoreItem("EachItem", 12.00m);
+            Assert.Equal("EachItem", storeItem.Name);
+            Assert.Equal(12.00m, storeItem.Price);
+        }
+
+        [Fact]
+        public void StoreItem_TestWeightedItemProperties()
+        {
+            StoreItem storeItem = new StoreItem("WeightedItem", 2.99m);
+            Assert.Equal("WeightedItem", storeItem.Name);
+            Assert.Equal(2.99m, storeItem.Price);
+        }
+    }
+}
