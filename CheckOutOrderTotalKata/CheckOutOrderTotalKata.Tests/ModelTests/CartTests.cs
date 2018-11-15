122292c50bc6ce6e6da5c80ddfcd65a870803ee8
ttmoreland <tristantmoreland@gmail.com>
2018-11-14 21:05:05 -0600

    Added property setting test for cart

---
+++ CheckOutOrderTotalKata/CheckOutOrderTotalKata.Tests/ModelTests/CartTests.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using Xunit;
+using CheckOutOrderTotalKata.Models;
+
+namespace CheckOutOrderTotalKata.Tests
+{
+    public class CartTests
+    {
+        [Fact]
+        public void Cart_TestProperties()
+        {
+            Cart cart = new Cart(100m);
+            Assert.Equal(100m, cart.Total);
+        }
+    }
+}
